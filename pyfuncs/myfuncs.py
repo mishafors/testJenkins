46be1d1b0fdebf6eaafa03e141d2a0a6e4ba123f
mishafors <towafit@gmail.com>
2015-05-23 16:59:07 +0200

    Added another test

--- pyfuncs/myfuncs.py
+++ pyfuncs/myfuncs.py
@@ -1,2 +1,5 @@
 def substract(arg0, arg1):
 	return arg0 - arg1
+
+def add(arg0, arg1):
+	return arg0 + arg1
--- pyfuncs/pytests/testMyFuncs.py
+++ pyfuncs/pytests/testMyFuncs.py
@@ -1,3 +1,6 @@
 import pyfuncs.myfuncs			
 def test_substract():
 	assert pyfuncs.myfuncs.substract(1,2) == -1
+
+def test_add()):
+	assert pyfuncs.myfuncs.add(1,2) == 3
